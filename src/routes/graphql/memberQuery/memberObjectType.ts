956a416811c1a8a0f03008277ca10f777de12ca3
Aliaksandr <sanya55bychyk@gmail.com>
2023-07-23 12:59:49 +0200

    feat: add memberEnumType

--- src/routes/graphql/memberQuery/memberObjectType.ts
+++ src/routes/graphql/memberQuery/memberObjectType.ts
@@ -1,11 +1,28 @@
-import { GraphQLObjectType, GraphQLFloat, GraphQLInt } from "graphql";
+import { GraphQLObjectType, GraphQLFloat, GraphQLInt, GraphQLEnumType } from "graphql";
+import { MemberTypeId } from "../../member-types/schemas.js";
+
+const memberEnumType = new GraphQLEnumType({
+  name: 'MemberTypeId',
+  description: 'A MemberTypeId',
+  values: {
+    basic: {
+      value: MemberTypeId.BASIC,
+      description: 'basic',
+    },
+    business: {
+      value: MemberTypeId.BUSINESS,
+      description: 'business',
+    }
+  }
+})
 
 const memberObjectType = new GraphQLObjectType({
   name: 'MemberType',
   description: 'A MemberType',
   fields: () => ({
     id: {
-      // type: 
+      type: memberEnumType,
+      description: 'The id',
     },
     discount: {
       type: GraphQLFloat,
