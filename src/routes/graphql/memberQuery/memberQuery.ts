4673191eaf6568e0d7beae66a54a9e1171381697
Aliaksandr <sanya55bychyk@gmail.com>
2023-07-23 14:06:45 +0200

    feat: implement memberQuery

--- src/routes/graphql/memberQuery/memberQuery.ts
+++ src/routes/graphql/memberQuery/memberQuery.ts
@@ -1,9 +1,30 @@
-import memberObjectType from "./memberObjectType.js";
+import { MemberType } from "@prisma/client";
+import memberObjectType, { memberEnumType } from "./memberObjectType.js";
+import IContext from "../types/IContext.js";
+import memberObjectTypeList from "./memberObjectTypeList.js";
 
 const memberQuery = {
   memberType: {
     type: memberObjectType,
-  }
+    args: {
+      id: {
+        type: memberEnumType,
+      },
+      resolve: async (_source, args: MemberType, context: IContext) => {
+        return await context.prisma.memberType.findUnique({ 
+          where: {
+            id: args.id,
+          },
+        });
+      },
+    },
+  },
+  memberTypes: {
+    type: memberObjectTypeList,
+    resolve: async (_source, _args, context: IContext) => {
+      return await context.prisma.memberType.findMany();
+    }
+  },
 };
 
 export default memberQuery;
