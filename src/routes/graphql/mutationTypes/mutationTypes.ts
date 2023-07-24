6685b18cd5fa8a478671f4442edbe873b038993d
Aliaksandr <sanya55bychyk@gmail.com>
2023-07-24 15:22:57 +0200

    feat: add deletePost

--- src/routes/graphql/mutationTypes/mutationTypes.ts
+++ src/routes/graphql/mutationTypes/mutationTypes.ts
@@ -2,11 +2,13 @@ import { GraphQLObjectType } from "graphql";
 import createPost from "./createPost/createPost.js";
 import createUser from "./createUser/createUser.js";
 import createProfile from "./createProfile/createProfile.js";
+import deletePost from "./deletePost/deletePost.js";
 
 const mutationTypes = new GraphQLObjectType({
   name: 'Mutation',
   fields: () => ({
     ...createPost,
+    ...deletePost,
     ...createUser,
     ...createProfile,
   }),
